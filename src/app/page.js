25dc9189c0ac7c13f6b0dfc045726b788821a365
Caroline Urbainski <carolineurb.dev@outlook.com>
2026-06-30 14:37:38 -0300

    pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 import {CardPost}  from "./components/CardPost";
+import logger from "../logger";
+import styles from './page.module.css'
+import Link from "next/link";
 /*  const post ={
     
             "id": 1,
@@ -14,8 +17,8 @@ import {CardPost}  from "./components/CardPost";
                 "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
             }
   }*/
-async function getAllPosts(){
-const response = await fetch('http://localhost:3042/posts');
+async function getAllPosts(page){
+const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
 if(!response.ok){
   logger.error('Erro ao buscar os posts');
   return [];
@@ -23,13 +26,18 @@ if(!response.ok){
 logger.info('Posts obtidos com sucesso');
 return response.json();
 }
-export default async function Home() {
-  const posts = await getAllPosts();
+export default async function Home({searchParams}) {
+  const currentPage = searchParams?.page ||1;
+  const {data:posts, prev,next} = await getAllPosts(currentPage);
   return (
-    <main>
+    <main className={styles.grid}>
     {posts.map((post) => (
       <CardPost key={post.id} post={post} />
     ))}
+    <div className={styles.links}>
+    {prev && <Link href={`/?page=${prev}`}>Anterior</Link>}
+    {next && <Link href={`/?page=${next}`}>Próximo</Link>}
+    </div>
     </main>
   );
 }
